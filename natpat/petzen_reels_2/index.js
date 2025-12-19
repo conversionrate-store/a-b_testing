482f570c37cceeb071e6fb19883f6ce5f9f6e84b
Oleksandr Sventukh <79708807+OSventukh@users.noreply.github.com>
2025-12-19 18:25:34 +0200

    Update index.js

--- natpat/petzen_reels_2/index.js
+++ natpat/petzen_reels_2/index.js
@@ -1,43 +1,43 @@
 (function() {
   "use strict";
-  const o = (t) => new Promise((e) => {
-    const c = document.querySelector(t);
-    c && e(c);
-    const n = new MutationObserver(() => {
+  const a = (t) => new Promise((e) => {
+    const l = document.querySelector(t);
+    l && e(l);
+    const i = new MutationObserver(() => {
       const r = document.querySelector(t);
-      r && (e(r), n.disconnect());
+      r && (e(r), i.disconnect());
     });
-    n.observe(document.documentElement, {
+    i.observe(document.documentElement, {
       childList: !0,
       subtree: !0
     });
-  }), w = ({ name: t, dev: e }) => {
+  }), y = ({ name: t, dev: e }) => {
     console.log(
       `%c EXP: ${t} (DEV: ${e})`,
       "background: #3498eb; color: #fccf3a; font-size: 20px; font-weight: bold;"
     );
-  }, y = (t) => {
+  }, S = (t) => {
     let e = setInterval(function() {
       typeof window.clarity == "function" && (clearInterval(e), window.clarity("set", t, "variant_1"));
     }, 1e3);
-  }, i = "https://conversionrate-store.github.io/a-b_images/natpat/video/reels_2", s = [
+  }, n = "https://conversionrate-store.github.io/a-b_images/natpat/video/reels_2", o = [
     {
-      previewImageSrc: `${i}/preview_1.gif`,
-      videoSrc: `${i}/reels_1.mp4`,
+      previewImageSrc: `${n}/preview_1.gif`,
+      videoSrc: `${n}/reels_1.mp4`,
       description: "Your drawer is full of gimmicks. Choose Natpat Pet Zen vapor stickers. Lightweight, travel-ready and effective real calm from one sticker instead of complicated contraptions"
     },
     {
-      previewImageSrc: `${i}/preview_2.gif`,
-      videoSrc: `${i}/reels_2.mp4`,
+      previewImageSrc: `${n}/preview_2.gif`,
+      videoSrc: `${n}/reels_2.mp4`,
       description: "Different triggers need different tools. NatPat Pet Zen vapor stickers handle thunder, fireworks, separation, and vet days with one consistent, collar-ready approach."
     },
     {
-      previewImageSrc: `${i}/preview_3.gif`,
-      videoSrc: `${i}/reels_3.mp4`,
+      previewImageSrc: `${n}/preview_3.gif`,
+      videoSrc: `${n}/reels_3.mp4`,
       description: "Stress hormones spiral during triggers. NatPat Petzen vapor stickers provide soothing aromatics linked to calmer physiology, helping your pet deescalate quickly and stay settled, fun"
     }
   ];
-  class S {
+  class I {
     constructor() {
       this.elements = null, this.init();
     }
@@ -48,41 +48,45 @@
       this.elements && this.elements.forEach((e) => this.updateCardItems(e));
     }
     updateCardItems(e) {
-      e.querySelectorAll(".card-item").forEach((n, r) => {
-        var g, m, f, h;
-        const d = n.querySelector(".card-gif"), p = n.querySelector("video"), a = n.querySelector(".card-box > p"), u = n.querySelector(".fullscreen-btn");
-        d && p && a && (console.log("Updating card item", (g = s[r]) == null ? void 0 : g.previewImageSrc), d.src = ((m = s[r]) == null ? void 0 : m.previewImageSrc) || "", p.src = ((f = s[r]) == null ? void 0 : f.videoSrc) || "", a.textContent = `${((h = s[r]) == null ? void 0 : h.description) || a.textContent}`, u && u.addEventListener("click", async () => {
+      e.querySelectorAll(".card-item").forEach((i, r) => {
+        var m, g, f, h;
+        const u = i.querySelector(".card-gif"), s = i.querySelector("video"), d = i.querySelector(".card-box > p"), p = i.querySelector(".fullscreen-btn");
+        u && s && d && (console.log("Updating card item", (m = o[r]) == null ? void 0 : m.previewImageSrc), u.src = ((g = o[r]) == null ? void 0 : g.previewImageSrc) || "", s.src = ((f = o[r]) == null ? void 0 : f.videoSrc) || "", d.textContent = `${((h = o[r]) == null ? void 0 : h.description) || d.textContent}`, p && p.addEventListener("click", async () => {
           var v;
-          const l = await o(
+          const c = await a(
             "#fullscreen-video"
           );
-          l && (l.src = ((v = s[r]) == null ? void 0 : v.videoSrc) || "", l.play());
+          c && (console.log("video)", s), s.pause(), document.querySelectorAll("video").forEach((w) => {
+            w !== c && w.pause();
+          }), c.src = ((v = o[r]) == null ? void 0 : v.videoSrc) || "", setTimeout(() => {
+            c.play();
+          }, 100));
         }));
       });
     }
     async getTargetSection() {
       this.elements = [
-        await o("#in-action-cards"),
-        await o("#in-action-slider")
+        await a("#in-action-cards"),
+        await a("#in-action-slider")
       ];
     }
   }
-  const I = "";
-  w({ name: "New PetZen - Social Proof Reels", dev: "OS" }), y("exp_reels");
-  class _ {
+  const _ = "";
+  y({ name: "New PetZen - Social Proof Reels", dev: "OS" }), S("exp_reels");
+  class C {
     constructor() {
       this.init();
     }
     init() {
-      this.isTargetPage() && (this.addStyles(), new S());
+      this.isTargetPage() && (this.addStyles(), new I());
     }
     isTargetPage() {
       return location.pathname.includes("zenpatch-pet");
     }
     addStyles() {
       const e = document.createElement("style");
-      e.textContent = I, document.head.appendChild(e);
+      e.textContent = _, document.head.appendChild(e);
     }
   }
-  new _();
+  new C();
 })();
