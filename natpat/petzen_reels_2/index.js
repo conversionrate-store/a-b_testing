daf57335e63c55f9d64aaa3e2ed2f55fb66e6b45
Oleksandr Sventukh <79708807+OSventukh@users.noreply.github.com>
2026-01-29 11:15:07 +0200

    Update index.js

--- natpat/petzen_reels_2/index.js
+++ natpat/petzen_reels_2/index.js
@@ -4,8 +4,8 @@
     const l = document.querySelector(t);
     l && e(l);
     const i = new MutationObserver(() => {
-      const r = document.querySelector(t);
-      r && (e(r), i.disconnect());
+      const n = document.querySelector(t);
+      n && (e(n), i.disconnect());
     });
     i.observe(document.documentElement, {
       childList: !0,
@@ -20,21 +20,21 @@
     let e = setInterval(function() {
       typeof window.clarity == "function" && (clearInterval(e), window.clarity("set", t, "variant_1"));
     }, 1e3);
-  }, n = "https://conversionrate-store.github.io/a-b_images/natpat/video/reels_2", o = [
+  }, r = "https://conversionrate-store.github.io/a-b_images/natpat/video/reels_2", o = [
     {
-      previewImageSrc: `${n}/preview_1.gif`,
-      videoSrc: `${n}/reels_1.mp4`,
-      description: "Your drawer is full of gimmicks. Choose Natpat Pet Zen vapor stickers. Lightweight, travel-ready and effective real calm from one sticker instead of complicated contraptions"
+      previewImageSrc: `${r}/preview_1.gif`,
+      videoSrc: `${r}/reels_1.mp4`,
+      description: "Car rides used to be, panting, drooling, shaking the whole thing. I put one of those calming stickers in her locket before we left and she just, curled up."
     },
     {
-      previewImageSrc: `${n}/preview_2.gif`,
-      videoSrc: `${n}/reels_2.mp4`,
-      description: "Different triggers need different tools. NatPat Pet Zen vapor stickers handle thunder, fireworks, separation, and vet days with one consistent, collar-ready approach."
+      previewImageSrc: `${r}/preview_2.gif`,
+      videoSrc: `${r}/reels_2.mp4`,
+      description: "My cat hates being groomed, like, hates it. Freezes up, won't come out of the carrier, the whole thing. I tried the calming patch before the appointment, and, The groomer actually said she did better than ever."
     },
     {
-      previewImageSrc: `${n}/preview_3.gif`,
-      videoSrc: `${n}/reels_3.mp4`,
-      description: "Stress hormones spiral during triggers. NatPat Petzen vapor stickers provide soothing aromatics linked to calmer physiology, helping your pet deescalate quickly and stay settled, fun"
+      previewImageSrc: `${r}/preview_3.gif`,
+      videoSrc: `${r}/reels_3.mp4`,
+      description: "My friend told me about these calming stickers and I was like, whatever, sure, put one in her little locket before a storm last week, and she actually settled? Like, she still noticed it, but she wasn't panicking. I'm not saying it's magic, but we actually slept through a thunderstorm."
     }
   ];
   class I {
@@ -48,17 +48,17 @@
       this.elements && this.elements.forEach((e) => this.updateCardItems(e));
     }
     updateCardItems(e) {
-      e.querySelectorAll(".card-item").forEach((i, r) => {
-        var m, g, f, h;
-        const u = i.querySelector(".card-gif"), s = i.querySelector("video"), d = i.querySelector(".card-box > p"), p = i.querySelector(".fullscreen-btn");
-        u && s && d && (console.log("Updating card item", (m = o[r]) == null ? void 0 : m.previewImageSrc), u.src = ((g = o[r]) == null ? void 0 : g.previewImageSrc) || "", s.src = ((f = o[r]) == null ? void 0 : f.videoSrc) || "", d.textContent = `${((h = o[r]) == null ? void 0 : h.description) || d.textContent}`, p && p.addEventListener("click", async () => {
-          var v;
+      e.querySelectorAll(".card-item").forEach((i, n) => {
+        var m, g, p, f;
+        const u = i.querySelector(".card-gif"), s = i.querySelector("video"), d = i.querySelector(".card-box > p"), h = i.querySelector(".fullscreen-btn");
+        u && s && d && (console.log("Updating card item", (m = o[n]) == null ? void 0 : m.previewImageSrc), u.src = ((g = o[n]) == null ? void 0 : g.previewImageSrc) || "", s.src = ((p = o[n]) == null ? void 0 : p.videoSrc) || "", d.textContent = `${((f = o[n]) == null ? void 0 : f.description) || d.textContent}`, h && h.addEventListener("click", async () => {
+          var w;
           const c = await a(
             "#fullscreen-video"
           );
-          c && (console.log("video)", s), s.pause(), document.querySelectorAll("video").forEach((w) => {
-            w !== c && w.pause();
-          }), c.src = ((v = o[r]) == null ? void 0 : v.videoSrc) || "", setTimeout(() => {
+          c && (console.log("video)", s), s.pause(), document.querySelectorAll("video").forEach((v) => {
+            v !== c && v.pause();
+          }), c.src = ((w = o[n]) == null ? void 0 : w.videoSrc) || "", setTimeout(() => {
             c.play();
           }, 100));
         }));
@@ -71,9 +71,9 @@
       ];
     }
   }
-  const _ = "";
+  const b = "";
   y({ name: "New PetZen - Social Proof Reels", dev: "OS" }), S("exp_reels");
-  class C {
+  class _ {
     constructor() {
       this.init();
     }
@@ -85,8 +85,8 @@
     }
     addStyles() {
       const e = document.createElement("style");
-      e.textContent = _, document.head.appendChild(e);
+      e.textContent = b, document.head.appendChild(e);
     }
   }
-  new C();
+  new _();
 })();
