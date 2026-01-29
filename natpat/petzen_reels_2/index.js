39d36fa5737a77e89c0df314db891ad926fb41f8
Oleksandr Sventukh <79708807+OSventukh@users.noreply.github.com>
2026-01-29 13:30:47 +0200

    Update index.js

--- natpat/petzen_reels_2/index.js
+++ natpat/petzen_reels_2/index.js
@@ -20,24 +20,24 @@
     let e = setInterval(function() {
       typeof window.clarity == "function" && (clearInterval(e), window.clarity("set", t, "variant_1"));
     }, 1e3);
-  }, r = "https://conversionrate-store.github.io/a-b_images/natpat/video/reels_2", o = [
+  }, o = "https://conversionrate-store.github.io/a-b_images/natpat/video/reels_2", r = [
     {
-      previewImageSrc: `${r}/preview_1.gif`,
-      videoSrc: `${r}/reels_1.mp4`,
+      previewImageSrc: `${o}/preview_1.gif`,
+      videoSrc: `${o}/reels_1.mp4`,
       description: "Car rides used to be, panting, drooling, shaking the whole thing. I put one of those calming stickers in her locket before we left and she just, curled up."
     },
     {
-      previewImageSrc: `${r}/preview_2.gif`,
-      videoSrc: `${r}/reels_2.mp4`,
+      previewImageSrc: `${o}/preview_2.gif`,
+      videoSrc: `${o}/reels_2.mp4`,
       description: "My cat hates being groomed, like, hates it. Freezes up, won't come out of the carrier, the whole thing. I tried the calming patch before the appointment, and, The groomer actually said she did better than ever."
     },
     {
-      previewImageSrc: `${r}/preview_3.gif`,
-      videoSrc: `${r}/reels_3.mp4`,
+      previewImageSrc: `${o}/preview_3.gif`,
+      videoSrc: `${o}/reels_3.mp4`,
       description: "My friend told me about these calming stickers and I was like, whatever, sure, put one in her little locket before a storm last week, and she actually settled? Like, she still noticed it, but she wasn't panicking. I'm not saying it's magic, but we actually slept through a thunderstorm."
     }
   ];
-  class I {
+  class b {
     constructor() {
       this.elements = null, this.init();
     }
@@ -49,17 +49,18 @@
     }
     updateCardItems(e) {
       e.querySelectorAll(".card-item").forEach((i, n) => {
-        var m, g, p, f;
-        const u = i.querySelector(".card-gif"), s = i.querySelector("video"), d = i.querySelector(".card-box > p"), h = i.querySelector(".fullscreen-btn");
-        u && s && d && (console.log("Updating card item", (m = o[n]) == null ? void 0 : m.previewImageSrc), u.src = ((g = o[n]) == null ? void 0 : g.previewImageSrc) || "", s.src = ((p = o[n]) == null ? void 0 : p.videoSrc) || "", d.textContent = `${((f = o[n]) == null ? void 0 : f.description) || d.textContent}`, h && h.addEventListener("click", async () => {
+        var g, m, p, f;
+        const u = i.querySelector(".card-gif"), c = i.querySelector("video"), d = i.querySelector(".card-box > p"), h = i.querySelector(".fullscreen-btn");
+        u && c && d && (console.log("Updating card item", (g = r[n]) == null ? void 0 : g.previewImageSrc), u.src = ((m = r[n]) == null ? void 0 : m.previewImageSrc) || "", c.src = ((p = r[n]) == null ? void 0 : p.videoSrc) || "", d.textContent = `${((f = r[n]) == null ? void 0 : f.description) || d.textContent}`, h && h.addEventListener("click", async () => {
           var w;
-          const c = await a(
+          console.log("Full screen button clicked");
+          const s = await a(
             "#fullscreen-video"
           );
-          c && (console.log("video)", s), s.pause(), document.querySelectorAll("video").forEach((v) => {
-            v !== c && v.pause();
-          }), c.src = ((w = o[n]) == null ? void 0 : w.videoSrc) || "", setTimeout(() => {
-            c.play();
+          s && (console.log("video", c), c.pause(), document.querySelectorAll("video").forEach((v) => {
+            v !== s && v.pause();
+          }), s.src = ((w = r[n]) == null ? void 0 : w.videoSrc) || "", console.log("fullScreenVideo", s), setTimeout(() => {
+            s.play();
           }, 100));
         }));
       });
@@ -71,21 +72,21 @@
       ];
     }
   }
-  const b = "";
+  const I = "";
   y({ name: "New PetZen - Social Proof Reels", dev: "OS" }), S("exp_reels");
   class _ {
     constructor() {
       this.init();
     }
     init() {
-      this.isTargetPage() && (this.addStyles(), new I());
+      this.isTargetPage() && (this.addStyles(), new b());
     }
     isTargetPage() {
       return location.pathname.includes("zenpatch-pet");
     }
     addStyles() {
       const e = document.createElement("style");
-      e.textContent = b, document.head.appendChild(e);
+      e.textContent = I, document.head.appendChild(e);
     }
   }
   new _();
