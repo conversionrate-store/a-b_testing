f9eb11a1db3df0db83f27c70981285a1356d442d
Oleksandr Sventukh <79708807+OSventukh@users.noreply.github.com>
2026-01-29 17:36:53 +0200

    Refactor promise handling and update video sources

--- natpat/petzen_reels_2/index.js
+++ natpat/petzen_reels_2/index.js
@@ -1,6 +1,6 @@
 (function() {
   "use strict";
-  const a = (s) => new Promise((e) => {
+  const c = (s) => new Promise((e) => {
     const r = document.querySelector(s);
     r && e(r);
     const t = new MutationObserver(() => {
@@ -11,29 +11,29 @@
       childList: !0,
       subtree: !0
     });
-  }), y = ({ name: s, dev: e }) => {
+  }), S = ({ name: s, dev: e }) => {
     console.log(
       `%c EXP: ${s} (DEV: ${e})`,
       "background: #3498eb; color: #fccf3a; font-size: 20px; font-weight: bold;"
     );
-  }, I = (s) => {
+  }, y = (s) => {
     let e = setInterval(function() {
       typeof window.clarity == "function" && (clearInterval(e), window.clarity("set", s, "variant_1"));
     }, 1e3);
-  }, o = "https://conversionrate-store.github.io/a-b_images/natpat/video/reels_2", c = [
+  }, n = "https://conversionrate-store.github.io/a-b_images/natpat/video/reels_2", l = [
     {
-      previewImageSrc: `${o}/preview_1.gif`,
-      videoSrc: `${o}/reels_1.mp4`,
+      previewImageSrc: `${n}/preview_1.gif`,
+      videoSrc: `${n}/reels_1.mp4`,
       description: "Car rides used to be, panting, drooling, shaking the whole thing. I put one of those calming stickers in her locket before we left and she just, curled up."
     },
     {
-      previewImageSrc: `${o}/preview_2.gif`,
-      videoSrc: `${o}/reels_2.mp4`,
+      previewImageSrc: `${n}/preview_2.gif`,
+      videoSrc: `${n}/reels_2.mp4`,
       description: "My cat hates being groomed, like, hates it. Freezes up, won't come out of the carrier, the whole thing. I tried the calming patch before the appointment, and, The groomer actually said she did better than ever."
     },
     {
-      previewImageSrc: `${o}/preview_3.gif`,
-      videoSrc: `${o}/reels_3.mp4`,
+      previewImageSrc: `${n}/preview_3.gif`,
+      videoSrc: `${n}/reels_3.mp4`,
       description: "My friend told me about these calming stickers and I was like, whatever, sure, put one in her little locket before a storm last week, and she actually settled? Like, she still noticed it, but she wasn't panicking. I'm not saying it's magic, but we actually slept through a thunderstorm."
     }
   ];
@@ -48,7 +48,7 @@
       this.elements && this.elements.forEach((e) => this.updateCardItems(e));
     }
     async stopAutoSlide() {
-      this.slider = await a("#in-action-slider .slick-initialized"), this.slider && this.slider.slick.slickPause();
+      this.slider = await c("#in-action-slider .slick-initialized"), this.slider && this.slider.slick.slickPause();
     }
     pauseSlider() {
       this.slider && this.slider.slick.slickPause();
@@ -79,55 +79,31 @@
     }
     updateCardItems(e) {
       e.querySelectorAll(".card-item").forEach((t, i) => {
-        var p, g, w;
-        const l = t.querySelector(".card-gif"), n = t.querySelector("video"), u = t.querySelector(".card-box > p"), m = t.querySelector(".fullscreen-btn");
-        l && n && u && (l.src = ((p = c[i]) == null ? void 0 : p.previewImageSrc) || "", n.src = ((g = c[i]) == null ? void 0 : g.videoSrc) || "", n.load(), u.textContent = `${((w = c[i]) == null ? void 0 : w.description) || u.textContent}`, this.setupVideoAutoPause(n), m && m.addEventListener("click", async () => {
-          var f;
-          const d = await a(
+        var m, p, f;
+        const u = t.querySelector(".card-gif"), a = t.querySelector("video"), d = t.querySelector(".card-box > p"), h = t.querySelector(".fullscreen-btn");
+        u && a && d && (u.src = ((m = l[i]) == null ? void 0 : m.previewImageSrc) || "", a.src = ((p = l[i]) == null ? void 0 : p.videoSrc) || "", a.load(), d.textContent = `${((f = l[i]) == null ? void 0 : f.description) || d.textContent}`, this.setupVideoAutoPause(a), h && h.addEventListener("click", async () => {
+          var g;
+          const o = await c(
             "#fullscreen-video"
           );
-          d && (n.pause(), document.querySelectorAll("video").forEach((S) => {
-            S !== d && S.pause();
-          }), d.src = ((f = c[i]) == null ? void 0 : f.videoSrc) || "", d.load(), setTimeout(() => {
-            d.play();
+          o && (a.pause(), document.querySelectorAll("video").forEach((w) => {
+            w !== o && w.pause();
+          }), o.src = ((g = l[i]) == null ? void 0 : g.videoSrc) || "", o.load(), setTimeout(() => {
+            o.play();
           }, 100));
         }));
       });
     }
     async getTargetSection() {
       this.elements = [
-        await a("#in-action-cards"),
-        await a("#in-action-slider")
+        await c("#in-action-cards"),
+        await c("#in-action-slider")
       ];
     }
   }
-  class b {
-    constructor() {
-      this.elements = null, this.init();
-    }
-    async init() {
-      await this.getTargetSection(), this.changeCardItems();
-    }
-    changeCardItems() {
-      this.elements && this.elements.forEach((e) => this.updateCardItems(e));
-    }
-    updateCardItems(e) {
-      e.querySelectorAll(".card-item").forEach((t, i) => {
-        var n;
-        const l = t.querySelector(".card-gif");
-        l && (l.src = ((n = c[i]) == null ? void 0 : n.previewImageSrc) || "");
-      });
-    }
-    async getTargetSection() {
-      this.elements = [
-        await a("#in-action-cards"),
-        await a("#in-action-slider")
-      ];
-    }
-  }
-  const k = "";
-  y({ name: "New PetZen - Social Proof Reels", dev: "OS" }), I("exp_reels");
-  class h {
+  const b = "";
+  S({ name: "New PetZen - Social Proof Reels", dev: "OS" }), y("exp_reels");
+  class k {
     constructor() {
       this.init();
     }
@@ -139,10 +115,10 @@
     }
     addStyles() {
       const e = document.createElement("style");
-      e.textContent = k, document.head.appendChild(e);
+      e.textContent = b, document.head.appendChild(e);
     }
   }
-  new b(), new h(), window.onload = () => {
-    new h();
+  window.onload = () => {
+    new k();
   };
 })();
